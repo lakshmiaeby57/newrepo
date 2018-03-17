efcb358129082dc41f36a8ececb46a6ada9156a8
lakshmiaeby57 <lakshmiaeby57@gmail.com>
2018-03-17 10:17:52 +0530

    again committed

---
+++ format.py
@@ -0,0 +1,11 @@
+for i in range(1,10):
+  print('{0:5d}  {1:5d}  {2:5d}  {3:5d}'.format(i,i**2,i**3,i**4))
+print('\n')
+for i in range(1,5):
+  print('{0:15b}  {1:15b}  {2:15b}  {3:15b}'.format(i,i**2,i**3,i**4))
+print('\n')
+for i in range(1,5):
+  print('{0:5o}  {1:5o}  {2:5o}  {3:5o}'.format(i,i**2,i**3,i**4))
+print('\n')
+for i in range(1,5):
+  print('{0:5e}  {1:5e}  {2:5e}  {3:5e}'.format(i,i**2,i**3,i**4))
